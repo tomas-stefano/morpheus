f9c3c30f6235c9f5a975283514c8992bb67bb7b7
Tomas D'Stefano <tomas_stefano@successoft.com>
2011-06-23 18:24:25 -0300

    Deleting old matchers

--- spec/matchers/include_task.rb
+++
@@ -1,9 +0,0 @@
-RSpec::Matchers.define :include_task do |task_names|
-  match do |tasks|
-    tasks.should be_instance_of(Array)
-    tasks.should_not be_empty
-
-    all_tasks = tasks.collect { |task| task.name.to_sym }
-    all_tasks.include?(task_name.to_sym)
-  end
-end
