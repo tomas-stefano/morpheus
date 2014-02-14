ca25eb21eaef7fb7e56840dd65f9a971d8518a5a
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-02-14 01:04:54 -0200

    Include core modules to Morpheus::FullTextSearch

--- lib/morpheus/full_text_search.rb
+++ lib/morpheus/full_text_search.rb
@@ -1,4 +1,12 @@
 module Morpheus
   module FullTextSearch
+    extend ActiveSupport::Concern
+
+    included do
+      include Core
+      include Persistence
+      include QueryMethods
+      include Relation
+    end
   end
 end
